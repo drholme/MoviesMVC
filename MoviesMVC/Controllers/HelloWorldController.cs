30f0e5ae99be58bf8fe4ce391dbceda0629f25d2
Darnita <darnita.holmes@gmail.com>
2018-09-25 19:19:47 -0400

    added created the HelloWorld Controller & then updated the /HelloWorld/ and /HelloWorld/Wecome methods

---
+++ MoviesMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoviesMVC.Controllers
+{
+        public class HelloWorldController : Controller
+        {
+        // 
+        // GET: /HelloWorld/ 
+        // This is the HelloWorld action method...
+        public string Index()
+            {
+                return "This is my <b>default</b> action...";
+            }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        // This is the Welcome action method...
+        public string Welcome()
+            {
+                return "This is the Welcome action method...";
+            }
+        }
+    }
